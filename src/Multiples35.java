898d8866bd262e5eccb9df7ea0b068e6c412479b
Alfonso Granados - vn540b5 <Alfonso.Granados@walmart.com>
2023-04-13 15:44:46 -0600

    multiple using stream

--- src/Multiples35.java
+++ src/Multiples35.java
@@ -1,8 +1,11 @@
+import java.util.stream.IntStream;
+
 public class Multiples35 {
 
     public static void main(String[] args) {
         int result = Multiples35.sum();
         System.out.println(result);
+        System.out.println(sumStream());
     }
 
     public static int sum(){
@@ -11,6 +14,13 @@ public class Multiples35 {
             if ((i % 3 == 0) || (i % 5 == 0))
                 sum += i;
         }
+        return sum;
+    }
+
+    public static int sumStream(){
+        int sum =IntStream.rangeClosed(1,1000)
+                .filter(i -> (i%3 == 0) || (i%5 == 0))
+                .reduce(0,(a,b)-> a+b); // Integer::sum
         return sum;
     }
 }
